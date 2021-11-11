3b76f2262bccdc7f26858957b9eaf39111e8dc6f
thefalked <giulianocrivelli@hotmail.com>
2021-11-11 15:51:12 -0400

    style: fixing style issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 
 import logo from "./logo.svg";
+
 import "./App.scss";
 
 const App: React.FC = () => (
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
+
       <p>
         Edit <code>src/App.tsx</code> and save to reload.
       </p>
+
       <a
         className="App-link"
         href="https://reactjs.org"
